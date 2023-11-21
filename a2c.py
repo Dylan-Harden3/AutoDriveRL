c060fc88e20a43ee8edb8f562cce31a5130d3aa8
prakcoin <prakhar2@gmail.com>
2023-11-20 22:42:15 -0600

    QOL + minor changes

--- a2c.py
+++ a2c.py
@@ -1,10 +1,6 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import gymnasium as gym
 import time
-from torch.optim import Adam
 
 import tensorflow as tf
 from tensorflow import keras
@@ -16,18 +12,20 @@ class A2C():
     def __init__(self, env, steps, hidden_neurons, lr, gamma):
         self.env = env
         self.steps = steps
-        self.actor_critic = self.a2c_network(self.env.observation_space.shape[0], hidden_neurons, self.env.action_space.n)
+        self.actor_critic = self.a2c_network(self.env.observation_space.shape, hidden_neurons, self.env.action_space.n)
         self.optimizer = keras.optimizers.Adam(learning_rate=lr)
         self.discount_factor = gamma
 
+
     def a2c_network(self, num_inputs, num_hidden, num_actions):
-        inputs = layers.Input(shape=(num_inputs,), name="input_layer")
+        inputs = layers.Input(num_inputs, name="input_layer")
         common = layers.Dense(num_hidden, activation="relu", name="common_layer")(inputs)
         actor = layers.Dense(num_actions, activation="softmax", name="actor_layer")(common)
         critic = layers.Dense(1, name="critic_layer")(common)
 
         model = keras.Model(inputs=inputs, outputs=[actor, critic])
         return model
+
 
     def select_action(self, state):
         state = tf.convert_to_tensor(state, dtype=tf.float64)
@@ -37,30 +35,31 @@ class A2C():
         ego_probs /= ego_probs.sum()
         action = np.random.choice(len(ego_probs), p=ego_probs)
 
-        return action, ego_probs[action], value
+        return action, ego_probs[action], value[0]
+
 
     def update_actor_critic(self, advantage, prob, value, tape):
         actor_loss = tf.reduce_mean(self.actor_loss(advantage, prob))
         critic_loss = tf.reduce_mean(self.critic_loss(advantage, value))
-
         loss = actor_loss + critic_loss
 
         grads = tape.gradient(loss, self.actor_critic.trainable_variables, unconnected_gradients=tf.UnconnectedGradients.ZERO)
         self.optimizer.apply_gradients(zip(grads, self.actor_critic.trainable_variables))
 
+
     def train_episode(self):
         state, _ = self.env.reset()
         total_reward = 0
-        for _ in range(self.steps):
+        total_steps = 0
+        for step in range(1, self.steps + 1):
             with tf.GradientTape() as tape:
+                total_steps = step
                 action, prob, value = self.select_action(state)
                 next_state, reward, done, _, _ = self.env.step(action)
 
                 next_state_tensor = tf.convert_to_tensor(next_state)
                 _, next_value = self.actor_critic(next_state_tensor)
-
                 next_value = next_value[0]
-                value = value[0]
 
                 if done:
                     next_value = next_value * 0
@@ -74,15 +73,18 @@ class A2C():
 
                 state = next_state
 
-        return total_reward
+        return total_reward, total_steps
+
 
     def actor_loss(self, advantage, prob):
         loss = -tf.math.log(prob) * advantage
         return loss
 
+
     def critic_loss(self, advantage, value):
         loss = -advantage * value
         return loss
+
 
     def __str__(self):
         return "A2C"
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 import gymnasium as gym
 import time
 
 from ddqn import DDQN
 from a2c import A2C
-from torch.optim import Adam
 
 
 
 def main():
     env = gym.make('highway-v0', render_mode='human')
-    num_episodes = 500
 
-    actor_critic_agent = A2C(env=env, steps=1000, hidden_neurons=128, lr=0.001, gamma=0.99)
+    # Hyperparameters
+    num_episodes = 2000
+    steps = 1000
+    neurons = 128
+    lr = 0.001
+    gamma = 0.99
+
+    actor_critic_agent = A2C(env=env, steps=steps, hidden_neurons=neurons, lr=lr, gamma=gamma)
     
+    start = time.time()
     for episode in range(1, num_episodes + 1):
+        reward, steps = actor_critic_agent.train_episode()
         print("Episode: ", episode)
-        print("Episode Reward: ", actor_critic_agent.train_episode())
-
+        print("Reward:", reward, "| Steps:", steps)
+    end = time.time()
+    print("Finished in:", end - start, "seconds")
+    
 if __name__ == "__main__":
     main()
